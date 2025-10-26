de85881ca6b4f6f1ecf329e01c56bd5754663544
L3GJ0N <dome.muller@gmail.com>
2025-10-26 12:43:03 +0100

    Add my answers for Git Pointers exercise

--- answers.py
+++ answers.py
@@ -22,21 +22,21 @@ student_id = "Your Student ID Here"
 
 # Your Answers (fill in all 15 questions)
 answers = {
-    1: "",  # Question 1
-    2: "",  # Question 2
-    3: "",  # Question 3
-    4: "",  # Question 4
-    5: "",  # Question 5
-    6: "",  # Question 6
-    7: "",  # Question 7
-    8: "",  # Question 8
-    9: "",  # Question 9
-    10: "",  # Question 10
-    11: "",  # Question 11
-    12: "",  # Question 12
-    13: "",  # Question 13
-    14: "",  # Question 14
-    15: "",  # Question 15
+    1: "A",  # Question 1
+    2: "D",  # Question 2
+    3: "B",  # Question 3
+    4: "A",  # Question 4
+    5: "C",  # Question 5
+    6: "B",  # Question 6
+    7: "A",  # Question 7
+    8: "B",  # Question 8
+    9: "B",  # Question 9
+    10: "B",  # Question 10
+    11: "B",  # Question 11
+    12: "B",  # Question 12
+    13: "B",  # Question 13
+    14: "B",  # Question 14
+    15: "B",  # Question 15
 }
 
 # Do not modify below this line
